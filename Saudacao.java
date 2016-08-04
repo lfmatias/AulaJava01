f916d191a4ab15de71d0d41d1c08affc8ba4423d
Luiz Flavio Matias <luiz.dsantos@b2wdigital.com>
2016-08-04 09:07:23 -0300

    Incluimos agora a utilização do JOptionPane

--- Saudacao.java
+++ Saudacao.java
@@ -1,8 +1,20 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 class Saudacao {
 
 	public static void	main (String args[]){
 
-			System.out.println(args[0] + " Bem vindo! - @do");	
+		System.out.print("Digite o nome @do ");
+
+		Scanner entrada = new Scanner(System.in);
+		
+		//String nome = entrada.nextLine();
+		String nome = JOptionPane.showInputDialog("Digite o nome do @do");
+
+		System.out.println("O nome do @do é " + nome);
+
+		System.out.println(" Bem vindo! - @do");
 
 	}
 
